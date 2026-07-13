72ddf2a659daabcc570f7cac91beed167c7b2af1
sobok2026 <sobok2026@gmail.com>
2026-07-14 00:10:47 +0900

    wip

--- apps/web/src/utils/api-request.ts
+++ apps/web/src/utils/api-request.ts
@@ -1,14 +1,9 @@
 import { PROBLEM } from '@sobok/contracts'
-import { CookieKey } from '@sobok/http/cookie'
 import { isProblemType } from '@sobok/http/problem-details'
-import Cookies from 'js-cookie'
 
 import { fetchResponseData, ProblemDetailsError } from '@/utils/fetch-response'
 
 type SearchParamValue = boolean | number | string | number[] | string[] | null | undefined
-
-let authRefreshPromise: Promise<boolean> | null = null
-const AUTH_REFRESH_PATH = '/api/v1/auth/refresh'
 
 export class UserVisibleError extends Error {
   readonly name = 'UserVisibleError'
@@ -36,17 +31,7 @@ export async function fetchAPIData<T>(
   input: string | Request | URL,
   init?: RequestInit,
 ): Promise<{ data: T; response: Response }> {
-  const request = new Request(input, init)
-
-  try {
-    return await fetchResponseData<T>(request.clone())
-  } catch (error) {
-    if (isAuthenticationRequiredError(error) && shouldRefreshAuthCookies(request) && (await refreshAuthCookies())) {
-      return await fetchResponseData<T>(request.clone())
-    }
-
-    throw error
-  }
+  return await fetchResponseData<T>(new Request(input, init))
 }
 
 export function isAuthenticationRequiredError(error: unknown): boolean {
@@ -55,42 +40,4 @@ export function isAuthenticationRequiredError(error: unknown): boolean {
     error.status === 401 &&
     isProblemType(error.type, PROBLEM.AUTHENTICATION_REQUIRED.slug)
   )
-}
-
-function isAuthRefreshRequest(request: Request): boolean {
-  const requestURL = new URL(request.url)
-
-  return requestURL.origin === window.location.origin && requestURL.pathname === AUTH_REFRESH_PATH
-}
-
-async function refreshAuthCookies(): Promise<boolean> {
-  if (!authRefreshPromise) {
-    authRefreshPromise = fetch(AUTH_REFRESH_PATH, {
-      method: 'POST',
-      cache: 'no-store',
-    })
-      .then((response) => response.ok)
-      .catch(() => false)
-      .finally(() => {
-        authRefreshPromise = null
-      })
-  }
-
-  return authRefreshPromise
-}
-
-function shouldRefreshAuthCookies(request: Request): boolean {
-  if (typeof window === 'undefined' || isAuthRefreshRequest(request)) {
-    return false
-  }
-
-  if (Cookies.get(CookieKey.AUTH_HINT) !== '1') {
-    return false
-  }
-
-  if (request.credentials === 'same-origin') {
-    return new URL(request.url).origin === window.location.origin
-  }
-
-  return false
 }
