64f8ee14ebd915822cf888b3282b6df0b29aebbd
sobok2026 <sobok2026@gmail.com>
2026-07-14 00:30:39 +0900

    Update server.ts

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -2,5 +2,4 @@
 // 절대 들어가면 안 된다. 명시적으로 이 specifier로만 접근하므로 클라 파일에 새어들면 리뷰에서 드러난다.
 export { type Auth, auth, type Session, type SessionUser } from './auth'
 export { BBATON_PROVIDER_ID } from './bbaton'
-export { cleanupInactiveUsers } from './cleanup-inactive-users'
 export { refreshSessionCookies } from './session-cookie'
